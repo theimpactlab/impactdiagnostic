76e65ed41e265fe090cdead69c421f2d93f9f479
theimpactlab <ryan@theimpactlab.co.uk>
2024-12-19 23:57:36 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,19 @@ import type { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+  
+  try {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
 
-  if (!session && req.nextUrl.pathname !== '/login') {
-    const redirectUrl = req.nextUrl.clone()
-    redirectUrl.pathname = '/login'
-    return NextResponse.redirect(redirectUrl)
+    if (!session && req.nextUrl.pathname !== '/login') {
+      const redirectUrl = req.nextUrl.clone()
+      redirectUrl.pathname = '/login'
+      return NextResponse.redirect(redirectUrl)
+    }
+  } catch (error) {
+    console.error('Middleware error:', error)
   }
 
   return res
